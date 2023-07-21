786b97f201d414531b5ad29ecacdbe1dfe6df333
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-21 20:36:16 +0300

    Export Hello Screen from App.js

---
+++ components/HelloWorld.js
@@ -0,0 +1,53 @@
+import {View, Text, StatusBar, StyleSheet, TouchableOpacity, Image, Dimensions} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+function HelloWorldScreen(windowWidth, windowHeight) {
+    
+    const navigation = useNavigation();
+    const handleTextPress = () => {
+      navigation.navigate('Welcome');
+    };
+  
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleTextPress}>
+        <Text style={styles.text}>Hello World!</Text>
+        <Image
+            source={require('./../assets/gifs/tv.gif')}
+            style={styles.gif}
+            autoplay
+            loop
+          />
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    gifContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    gif: {
+      width: windowWidth * 0.6, // Adjust the width as needed
+      height: windowHeight * 0.4, // Adjust the height as needed
+    },
+    text: {
+      fontFamily: 'press-start',
+      fontSize: 35,
+      textAlign: 'center', // Align text in the center
+    },
+  });
+  
+  export default HelloWorldScreen;
