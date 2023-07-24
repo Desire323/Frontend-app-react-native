c4e395c11941577f5b34c2d67d199163689b40f5
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-24 20:19:07 +0300

    Export theme styles

---
+++ components/animations/styles/OppenheimerStyle.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const oppenheimerStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: "black",
+    },
+    text: {
+      fontFamily: 'oppenheimer',
+      fontSize: 35,
+      color: "#FF7F50",
+      alignSelf: 'center',
+      textAlign: 'center',
+      paddingHorizontal: 8,
+      lineHeight: 50,
+    },
+    question: {
+      fontFamily: 'press-start',
+      fontSize: 90,
+      alignSelf: 'center',
+      textAlign: 'center',
+      paddingHorizontal: 8,
+      // lineHeight: 50,
+    },
+  });
+
+export default oppenheimerStyles;
