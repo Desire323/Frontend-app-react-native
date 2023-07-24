f490109d445ad4c1660420b1052d867c0ef0fced
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-24 20:20:16 +0300

    Add StyleManager for themed styles

---
+++ components/animations/styles/StyleManager.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import oppenheimerStyles from './OppenheimerStyle';
+import barbieStyles from './BarbieStyle';
+
+
+function StyleManager({theme}) {
+  
+    switch (theme) {
+      case 'Barbie':
+        return barbieStyles;
+      case 'Oppenheimer':
+        return oppenheimerStyles;
+    default:
+        return defaultStyles;
+  };
+}
+
+const defaultStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontFamily: 'press-start',
+      fontSize: 30,
+      flexWrap: 'wrap',
+      alignSelf: 'center',
+      textAlign: 'center',
+      paddingHorizontal: 8,
+      lineHeight: 50,
+      resizeMode: 'contain',
+    },
+    question: {
+      fontFamily: 'press-start',
+      fontSize: 90,
+      alignSelf: 'center',
+      textAlign: 'center',
+      paddingHorizontal: 8,
+      // lineHeight: 50,
+    },
+  });
+  
+export default StyleManager;
+    
