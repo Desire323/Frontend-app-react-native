9ead27b5748c7103999d8f26cfb3e645df606f56
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-24 20:30:02 +0300

    Add themed styles and animations in Fortune

--- components/Fortune.js
+++ components/Fortune.js
@@ -3,52 +3,58 @@ import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage'; // import from the correct package
 import fortune_random from './api/fortune_api';
+import Animation from './animations/Animation';
+import StyleManager from './animations/styles/StyleManager';
 
 function Fortune() {
   const navigation = useNavigation();
   const [fortune, setFortune] = useState(null);
+  const [animation, setAnimation] = useState(false);
+  const [theme, setTheme] = useState(null);
 
   useEffect(() => {
-    fetchFortune();
+    // fetchFortune();
   }, []);
 
   const fetchFortune = async () => {
     const token = await AsyncStorage.getItem('token');
     console.log(`\ntoken: ${token}`);
     const response = await fortune_random(token); // await the asynchronous call
-    console.log(`\nresponse: ${response}`);
+    // console.log(`\nresponse: ${JSON.stringify(response)}`);
+
+    if(typeof response === 'object' && response.status === 500){
+      await AsyncStorage.removeItem('token')
+      navigation.navigate("Welcome")
+    }
     if(response){ //if 200 OK
-      setFortune(response);
+      setAnimation(true);
+      setTheme(response.theme);
+      setFortune(response.wish);
+      console.log(`\n\n\nSTYLES: ${JSON.stringify(StyleManager({theme: theme}).text)}`);
+      setTimeout(() => {
+        setAnimation(false);
+      }, 3500);
     }
   };
 
   return (
-    <View style={styles.container}>
+    <View style={StyleManager({theme: theme}).container}>
       {fortune ? (
+        animation ? 
+          <Animation theme={theme}/> 
+          :
         <TouchableOpacity onPress={fetchFortune}>
-        <Text style={styles.text}>{fortune}</Text>
+        <Text style={StyleManager({theme: theme}).text}>{fortune}</Text>
         </TouchableOpacity>
       ) : (
-        <Text>Loading...</Text>
+        <TouchableOpacity onPress={fetchFortune}>
+        <Text style={StyleManager({theme: theme}).question}>?</Text>
+        </TouchableOpacity>
       )}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontFamily: 'press-start',
-    fontSize: 30,
-    alignSelf: 'center',
-    textAlign: 'center',
-    paddingHorizontal: 8,
-    lineHeight: 50,
-  },
-});
+
 
 export default Fortune;
