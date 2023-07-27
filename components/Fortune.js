943496c2acb244e65452c4ed9870d556bf170965
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-27 18:13:20 +0300

    Implement fortunes history

--- components/Fortune.js
+++ components/Fortune.js
@@ -1,36 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage'; // import from the correct package
-import fortune_random from './api/fortune_api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {fortune_random} from './api/fortune_api';
 import Animation from './animations/Animation';
 import StyleManager from './animations/styles/StyleManager';
+import { checkTokenExpiration } from './auth/authUtils';
 
 function Fortune() {
   const navigation = useNavigation();
   const [fortune, setFortune] = useState(null);
   const [animation, setAnimation] = useState(false);
   const [theme, setTheme] = useState(null);
+  const [styles, setStyles] = useState(StyleManager({theme: theme}));
+  
+  useEffect(() => {
+    handleTokenExpiration();
+  }, []);
 
   useEffect(() => {
-    // fetchFortune();
-  }, []);
+    setStyles(StyleManager({ theme: theme }));
+  }, [theme]);
+  
+  const fetchHistory = async () => {
+    navigation.navigate("History")
+  }
+
+  const handleTokenExpiration = async () => {
+    const isTokenValid = await checkTokenExpiration();
+
+    if (!isTokenValid) {
+        navigation.navigate('Welcome');
+    }};
 
   const fetchFortune = async () => {
     const token = await AsyncStorage.getItem('token');
-    console.log(`\ntoken: ${token}`);
-    const response = await fortune_random(token); // await the asynchronous call
-    // console.log(`\nresponse: ${JSON.stringify(response)}`);
+    const response = await fortune_random(token);
 
     if(typeof response === 'object' && response.status === 500){
       await AsyncStorage.removeItem('token')
       navigation.navigate("Welcome")
     }
-    if(response){ //if 200 OK
+  
+    if(response){
       setAnimation(true);
       setTheme(response.theme);
       setFortune(response.wish);
-      console.log(`\n\n\nSTYLES: ${JSON.stringify(StyleManager({theme: theme}).text)}`);
       setTimeout(() => {
         setAnimation(false);
       }, 3500);
@@ -38,18 +53,23 @@ function Fortune() {
   };
 
   return (
-    <View style={StyleManager({theme: theme}).container}>
+    <View style={styles.container}>
       {fortune ? (
         animation ? 
           <Animation theme={theme}/> 
           :
         <TouchableOpacity onPress={fetchFortune}>
-        <Text style={StyleManager({theme: theme}).text}>{fortune}</Text>
+        <Text style={styles.text}>{fortune}</Text>
         </TouchableOpacity>
       ) : (
-        <TouchableOpacity onPress={fetchFortune}>
-        <Text style={StyleManager({theme: theme}).question}>?</Text>
-        </TouchableOpacity>
+        <View>
+          <TouchableOpacity onPress={fetchFortune}>
+            <Text style={styles.question}>?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={fetchHistory}>
+            <Text style={StyleSheet.compose(styles.text, {paddingTop:100})}>history</Text>
+          </TouchableOpacity>
+        </View>
       )}
     </View>
   );
