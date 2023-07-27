e930513623f222956706b03b998a134794ea6bfb
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-27 17:38:53 +0300

    Add custom button componnt

---
+++ components/Button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, StyleSheet, Pressable } from 'react-native';
+
+function Button(props) {
+  const { onPress, title = 'Save' } = props;
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    marginHorizontal: 20,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
+
+export default Button;
