c072975fdb12095791a2f81569123cb9078e0e54
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-28 15:04:37 +0300

    Add fortune_history_last request in fortune_api

--- components/api/fortune_api.js
+++ components/api/fortune_api.js
@@ -22,4 +22,15 @@ const fortune_history = async (token) => {
     }
 }
 
-export { fortune_random, fortune_history };
+const fortune_history_last = async (token) => {
+    try {
+        const response = await ApiManager.get('/fortune/history/last', {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+        return response.data ? response.data : response;
+    } catch (error) {
+        return error.response && error.response.data ? error.response.data : error;
+    }
+}
+
+export { fortune_random, fortune_history, fortune_history_last };
