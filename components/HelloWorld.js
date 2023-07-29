3b664e30d615cd1156a126a3463b55324023e930
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-29 14:05:08 +0300

    Update 'empty' greeting in Hello screen

--- components/HelloWorld.js
+++ components/HelloWorld.js
@@ -4,7 +4,6 @@ import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { checkTokenExpiration } from './auth/authUtils'; 
 
-
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -18,7 +17,7 @@ function HelloWorldScreen() {
   useEffect(() => {
     const getName = async () => {
       const name = await AsyncStorage.getItem('firstname');
-      setName(name);
+      setName(name ? name : 'World');
     };
     getName();
   }, [AsyncStorage.getItem('firstname')]);
