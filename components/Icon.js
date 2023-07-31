f33fe546f9aa4cf1996e54718060be40a7758f57
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-31 11:59:23 +0300

    Switch icons from Entypo to MaterialCommunityIcons

--- components/Icon.js
+++ components/Icon.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Entypo } from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 
 function Icon({ onPress, icon, color, size}) {
     return (
         <TouchableOpacity onPress={onPress}>
-            <Entypo name={icon} size={size ? size : 28} color={color ? color : '#f1f1f1'}/>
+            <MaterialCommunityIcons name={icon} size={size ? size : 28} color={color ? color : '#f1f1f1'}/>
         </TouchableOpacity> 
         )
 }
