fc9511689b0709a87adadc061060c4607ae08bb4
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-31 12:03:38 +0300

    Update TabBar

--- components/TabBar.js
+++ components/TabBar.js
@@ -1,18 +1,40 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Settings from './components/Settings';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, View, Text } from 'react-native';
+import Settings from './Settings';
+import Icon from './Icon';
+import { useNavigation } from '@react-navigation/native';
 
-const Tab = createBottomTabNavigator();
 
 function TabBar() {
-
+    const navigation = useNavigation();
 
     return (
-        <NavigationContainer>
-            <Tab.Navigator>
-          {/* <Tab.Screen name="Home" component={HelloWorldScreen} /> */}
-          <Tab.Screen name="Settings" component={Settings} />
-         </Tab.Navigator>
-        </NavigationContainer>
+        <View style={styles.container}>  
+            <Icon icon={"account"} color={"black"} onPress={() => navigation.navigate("Profile")}/>
+            
+            <Text>Fortunes</Text>
+            <Icon icon={"crystal-ball"} color={"black"} onPress={() => navigation.navigate("Fortune")}/>
+            <Text>Feed</Text>
+            <Icon icon={"cog"} color={"black"} onPress={() => navigation.navigate("Settings")}/>
+
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        height: 50,
+        // backgroundColor: 'red',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'flex-start',
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+});
+
+export default TabBar;
