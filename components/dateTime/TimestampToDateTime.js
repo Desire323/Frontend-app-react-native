0fe33384c063859d927bc684a43d3fb09ef3df16
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-08-03 21:32:39 +0300

    Update date handling in TimestampToDateTime

--- components/dateTime/TimestampToDateTime.js
+++ components/dateTime/TimestampToDateTime.js
@@ -26,17 +26,36 @@ function TimestampToDateTime({ timestamp, style }) {
   // Check if the year is the same but the date is not today
   const isSameYear = year === now.getFullYear();
 
+  // Check if the date is within the same week
+  const isSameWeek =
+    Math.abs(date - now) < 7 * 24 * 60 * 60 * 1000 &&
+    date.getDay() === now.getDay();
+
+  // Array of month names
+  const monthNames = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+
+  // Function to add leading zero to minutes
+  const padZero = (value) => {
+    return value.toString().padStart(2, '0');
+  };
+
   let displayText;
 
   if (isToday) {
-    displayText = `${hours}:${minutes}`;
+    displayText = `${hours}:${padZero(minutes)}`;
   } else if (isYesterday.getDate() === date.getDate()) {
-    displayText = `Yesterday ${hours}:${minutes}`;
-  } else if (isSameYear) {
+    displayText = `Yesterday ${hours}:${padZero(minutes)}`;
+  } else if (isSameWeek) {
     const dayOfWeek = date.toLocaleString('en', { weekday: 'short' });
-    displayText = `${dayOfWeek} ${hours}:${minutes}`;
+    displayText = `${dayOfWeek} ${hours}:${padZero(minutes)}`;
+  } else if (isSameYear) {
+    const monthName = monthNames[date.getMonth()]; // Get the localized month name
+    displayText = `${day} ${monthName} ${hours}:${padZero(minutes)}`;
   } else {
-    displayText = `${day}.${month}.${year} ${hours}:${minutes}`;
+    displayText = `${day}.${month}.${year} ${hours}:${padZero(minutes)}`;
   }
 
   return <Text style={style}>{displayText}</Text>;
