bc28408963743b295f4f8430fbdec97b437a295d
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-08-07 00:22:46 +0300

    Add style prop to FortuneMessage

--- components/FortuneMessage.js
+++ components/FortuneMessage.js
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import StyleManager from "./animations/styles/StyleManager";
 import { useState, useEffect } from "react";
 
-function FortuneMessage({fortune}) {
+function FortuneMessage({fortune, style}) {
     const [message, setMessage] = useState(null);
     const [theme, setTheme] = useState(null);
     const [styles, setStyles] = useState(StyleManager({theme: theme}));
@@ -19,7 +19,7 @@ function FortuneMessage({fortune}) {
     }, []);   
 
     return (
-      <View style={StyleSheet.compose(styles.container, {width: 200, maxHeight: 300, borderRadius: 25 })}>
+      <View style={StyleSheet.compose(styles.container, style)}>
           <TouchableOpacity onPress={() => console.log()}>
             <Text style={StyleSheet.compose(styles.text, {fontSize: styles.text.fontSize - 25, lineHeight: styles.text.lineHeight - 30 })}>{message}</Text>
           </TouchableOpacity>
